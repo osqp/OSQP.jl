5d1bac4417ed1df57caf1c5f6bbd979a187577aa
Twan Koolen <koolen.twan@gmail.com>
2018-02-12 18:21:53 -0500

    Hack to get constraint primals.

--- test/MathOptInterfaceOSQP.jl
+++ test/MathOptInterfaceOSQP.jl
@@ -21,14 +21,36 @@ MOIU.@model(OSQPCachingOptimizer, # modelname
     () # typedvectorfunctions
 )
 
+# FIXME: type piracy. Generalize and move to MOIU.
+MOI.canget(optimizer::MOIU.CachingOptimizer, ::MOI.ConstraintPrimal, ::Type{<:MOI.ConstraintIndex}) = true
+function MOI.get(optimizer::MOIU.CachingOptimizer, ::MOI.ConstraintPrimal, ci::MOI.ConstraintIndex{<:MOI.SingleVariable, <:Any})
+    f = MOI.get(optimizer, MOI.ConstraintFunction(), ci)
+    MOI.get(optimizer, MOI.VariablePrimal(), f.variable)
+end
+function MOI.get(optimizer::MOIU.CachingOptimizer, ::MOI.ConstraintPrimal, ci::MOI.ConstraintIndex{<:MOI.ScalarAffineFunction, <:Any})
+    f = MOI.get(optimizer, MOI.ConstraintFunction(), ci)
+    ret = f.constant
+    n = length(f.variables)
+    length(f.coefficients) == n || error()
+    for i = 1 : n
+        ret += f.coefficients[i] * MOI.get(optimizer, MOI.VariablePrimal(), f.variables[i])
+    end
+    ret
+end
+
+
 const config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
 
 @testset "Continuous linear problems" begin
     excludes = collect(keys(MOIT.contlineartests))
     deleteat!(excludes, findfirst(excludes, "linear1"))
+
+    # excludes = String[]
+    # push!(excludes, "linear8a") # OSQP returns :Primal_infeasible and doesn't provide duals
+    # push!(excludes, "linear8c") # OSQP returns :Dual_infeasible and doesn't provide primals
     optimizer = OSQPOptimizer()
     MOI.set!(optimizer, OSQPSettings.Verbose(), false)
     MOI.set!(optimizer, OSQPSettings.EpsAbs(), 1e-8)
-    MOI.set!(optimizer, OSQPSettings.EpsRel(), 1e-8)
+    MOI.set!(optimizer, OSQPSettings.EpsRel(), 1e-16)
     MOIT.contlineartest(MOIU.CachingOptimizer(OSQPCachingOptimizer{Float64}(), optimizer), config, excludes)
 end
