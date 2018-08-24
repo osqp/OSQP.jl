93871a1e862348cff4658938ac712ca43f14a97a
Lorenzo Stella <stellalo@amazon.com>
2018-08-25 00:01:59 +0200

    missing @compat

--- deps/build.jl
+++ deps/build.jl
@@ -49,7 +49,7 @@ struct FileMoveRule <: BinDeps.BuildStep
     src::AbstractString
     dest::AbstractString
 end
-Base.run(fc::FileMoveRule) = isfile(fc.dest) || mv(fc.src, fc.dest, force=true)
+@compat Base.run(fc::FileMoveRule) = isfile(fc.dest) || mv(fc.src, fc.dest, force=true)
 
 provides(SimpleBuild,
     (@build_steps begin
