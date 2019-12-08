43e53157d53e831c124eb320000ca4d27f7f0f68
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-12-08 15:34:19 -0500

    Make tests compatible with julia <v"1.2"

--- test/interface.jl
+++ test/interface.jl
@@ -2,7 +2,7 @@ using SparseArrays: SparseMatrixCSC, sparse
 using LinearAlgebra: I
 using OSQP: Ccsc, ManagedCcsc
 @testset "sparse matrix interface roundtrip" begin
-    jl = sparse(I(5))
+    jl = sparse(Matrix{Bool}(LinearAlgebra.I, 5, 5))
     mc = ManagedCcsc(jl)
     c = Ccsc(mc)
     jl2 = convert(SparseMatrixCSC, c)
