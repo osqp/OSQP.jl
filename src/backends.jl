62220d0a73f3a5211906b28cc70e3921f7c8a74c
Ian McInerney <ian.s.mcinerney@ieee.org>
2022-07-06 14:44:56 +0100

    Working builtin backend ccall

--- src/backends.jl
+++ src/backends.jl
@@ -1,22 +1,30 @@
 """
     OSQPBackend
 
-
+The supertype for all backend library implementations of OSQP to implement
+a library pointer type underneath.
 """
 abstract type OSQPBackend end
 
-@generated function osqp_ccall(func, b::backend, args...) where {backend <: OSQPBackend}
-    libname = getlibrary(backend())
 
-    return :(ccall((func, $(libname)), args...))
-    #return _osqp_ccall(func, b, args...)
+"""
+    osqp_ccall(func, b::backend, returnType, paramTypes, args...) where {backend <: OSQPBackend}
+
+Call a function `func` inside the OSQP backend library specified by `backend`. The remaining
+arguments are the same as a normal ccall.
+"""
+@generated function osqp_ccall(func, b::backend, returnType, paramTypes, args...) where {backend <: OSQPBackend}
+    return _osqp_ccall( func, b, returnType, paramTypes, args...)
 end
 
-function _osqp_ccall( func, ::Type{backend}, args...) where {backend <: OSQPBackend}
+function _osqp_ccall( func, ::Type{backend}, returnType, paramTypes, args...) where {backend <: OSQPBackend}
     libname = getlibrary(backend())
 
-    return :(ccall((func, $(libname)), args...))
+    return :(ccall((func, $(libname)), returnType, paramTypes, args...))
 end
+
+export OSQPBuiltin
+
 
 """
     OSQPBuiltin
