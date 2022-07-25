4f24408b90e08cf11446d8b8c6bd99fce0ab77c2
Ian McInerney <ian.s.mcinerney@ieee.org>
2022-07-25 10:29:48 +0100

    Update MKL backend package

--- algebras/OSQPMKL/src/OSQPMKL.jl
+++ algebras/OSQPMKL/src/OSQPMKL.jl
@@ -2,14 +2,18 @@ module OSQPMKL
 
 using OSQP_MKL_jll
 
-export OSQPMKL
+using OSQP
+using OSQP: OSQPAlgebra
+using OSQP: @cdefinition
+
+export OSQPMKLAlgebra
 
 """
-    OSQPMKL()
+    OSQPMKLAlgebra()
 
 Use the double precision MKL linear algebra backend and linear system solvers.
 """
-struct OSQPMKL <: OSQP.OSQPAlgebra end
+struct OSQPMKLAlgebra <: OSQPAlgebra end
 
 
 # The integer type is defined by the C library based on the size of ints on the
@@ -23,25 +27,20 @@ end
 ###############################################
 # Define the library functions                #
 ###############################################
-
-# Helper functions to wrap the library handles
-library(::Type{OSQPMKL}) = OSQP_MKL_jll.osqp_mkl
-librarysym(::Type{OSQPMKL}) = :(OSQP_MKL_jll.osqp_mkl)
-
 # Create a definition for all the functions present in the double precision library
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_capabilities
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_version
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_error_message
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_get_dimensions
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_set_default_settings
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_setup
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_solve
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_cleanup
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_warm_start
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_cold_start
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_update_data_vec
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_update_data_mat
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_update_settings
-@cdefinition OSQPMKL{Float64} Float64 Cc_int osqp_update_rho
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_capabilities
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_version
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_error_message
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_get_dimensions
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_set_default_settings
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_setup
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_solve
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_cleanup
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_warm_start
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_cold_start
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_update_data_vec
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_update_data_mat
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_update_settings
+@cdefinition OSQPMKLAlgebra osqp_mkl Float64 Cc_int osqp_update_rho
 
 end # module
